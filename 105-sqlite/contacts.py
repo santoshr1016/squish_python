6a0b77b60d45dc13f929dc8395db6c091f12e469
santoshr1016 <santy1016@gmail.com>
2020-05-22 08:09:54 +0530

    Add sqlite

---
+++ 105-sqlite/contacts.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+db = sqlite3.connect("contacts.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('Tim', 6545678, 'tim@email.com')")
+db.execute("INSERT INTO contacts VALUES('Brian', 1234, 'brian@myemail.com')")
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+
+# print(cursor.fetchall())
+
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+
+for name, phone, email in cursor:
+    print(name)
+    print(phone)
+    print(email)
+    print("-" * 20)
+
+cursor.close()
+db.commit()
+db.close()
+
+"""
+import sqlite3
+
+db = sqlite3.connect("contacts.sqlite")
+
+update_sql = "UPDATE contacts SET email = 'update@update.com'"
+update_cursor = db.cursor()
+update_cursor.execute(update_sql)
+print("{} rows updated".format(update_cursor.rowcount))
+
+print()
+print("Are connections the same: {}".format(update_cursor.connection == db))
+print()
+
+update_cursor.connection.commit()
+update_cursor.close()
+
+for name, phone, email in db.execute("SELECT * FROM contacts"):
+    print(name)
+    print(phone)
+    print(email)
+    print("-" * 20)
+
+db.close()
+"""
